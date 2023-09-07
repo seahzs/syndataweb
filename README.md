c3ca00aa9896cdad308436e297943df691347a5e
seahzs <seahzs@gmail.com>
2023-09-07 22:36:14 +0800

    7Sep

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # syndataweb
 Web UI for Synthetic Data Generation
+
+## Instructions
+- Install all dependencies stated in requirements.txt
+- Run app.py, then use web browser to go to [localhost:5000](localhost:5000) or [127.0.0.1:5000](127.0.0.1:5000)
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, BooleanField, SubmitField
+from wtforms import SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 from secrets import token_urlsafe
 import os
@@ -12,19 +12,16 @@ app = Flask(__name__)
 key = token_urlsafe(16)
 app.secret_key = key
 csrf = CSRFProtect(app)
-app.config['UPLOAD_FOLDER']='uploads'
+app.config['UPLOAD_FOLDER']='/uploads'
 
-data = {
-    'Name': ['John', 'Anna', 'Peter', 'Linda'],
-    'Age': [28, 24, 35, 32],
-    'City': ['New York', 'Paris', 'Berlin', 'London']
-}
-
-df = pd.DataFrame(data)
+test_data = {'Name': ['John', 'Anna', 'Peter', 'Linda'],
+            'Age': [28, 24, 35, 32],
+            'City': ['New York', 'Paris', 'Berlin', 'London']}
+data = pd.DataFrame(test_data)
 
 class DataForm(FlaskForm):
-    name = StringField('Your Name', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email Add', validators=[DataRequired()])
+    global data
+    column_drop = SelectField(label='Drop Column', choices=data.columns.values)
     submit = SubmitField('Submit Now')
 
 class UploadForm(FlaskForm):
@@ -37,33 +34,33 @@ def homepage():
 
 @app.route("/form", methods=['POST', 'GET'])
 def formpage():
+    global data
     if request.method == 'POST':
-        return render_template('result.html', title='Result', result=request.form['name'])
+        to_drop=request.form['column_drop']
+        data.drop(columns=[to_drop])
+        return render_template('result.html', title='Result', result=f'Column "{to_drop}" has been dropped')
     else:
         form=DataForm()
-        result=''
-        if form.validate_on_submit():
-            result=form.name.data
-            flash(form.name.data)
-            return redirect(url_for('home'))
         return render_template('form.html', title='Form', form=form)
 
 @app.route("/upload", methods=['POST', 'GET'])
 def uploadpage():
+    global data
     if request.method == 'POST':
-        f = request.files.get('file')
-        data_filename = secure_filename(f.filename)
-        data_file_path = os.path.join(app.config['UPLOAD_FOLDER'],data_filename)
-        f.save(data_file_path)
-        data = pd.read_csv(data_file_path, encoding='unicode_escape')
-        return render_template('result.html', title='Result', result=f'File {data_filename} has been uploaded')
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(os.getcwd(),'uploads',filename)
+        file.save(file_path)
+        data = pd.read_csv(file_path)
+        return render_template('result.html', title='Result', result=f'File "{filename}" has been uploaded')
     else:
         form=UploadForm()
         return render_template('upload.html', title='Upload', form=form)
 
 @app.route("/table")
 def tablepage():
-    return render_template('table.html', title='Table', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    sample=data.head()
+    return render_template('table.html', title='Table', tables=[sample.to_html(classes='data')], titles=sample.columns.values)
 
 if __name__ == '__main__':
     app.run(debug=True)
