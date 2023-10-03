0d483596521ce8655adf53db29300e5e4b1b358a
unknown <seahzs@gmail.com>
2023-10-03 21:50:19 +0800

    3Oct

--- pages/(2)_Data_Preparation.py
+++ pages/(2)_Data_Preparation.py
@@ -1,20 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-# Loads dataset and updates dataset list in sidebar
-datasets = {}
+# Loads dataset from session state and updates sidebar
+datasets={}
+if 'datasets' not in st.session_state:
+    st.error('Please load datasets to continue. *(See homepage)*')
+else:
+    datasets=st.session_state['datasets']
 with st.sidebar:
-    for dataset_name in ('Survey 2020', 'Survey 2021', 'Survey 2022'):
-        if dataset_name in st.session_state:
-            datasets[dataset_name]=st.session_state[dataset_name]
-    st.write('**Loaded Datasets:**')
-    st.info(", ".join(datasets.keys()))
+    st.info('**Datasets loaded:**')
+    for dataset in datasets:
+        st.write(f"- {dataset}")
 
 #Main Content
 st.write("### Data Preparation")
-st.write("Please select the preparation task(s) to be performed.")
-fix_headers = st.checkbox('**Rename Features** - *for column matching*')
-filter_common = st.checkbox('**Filter Common Features** - *drop columns not found across multiple datasets*')
-merge_headers = st.checkbox('**Combine Features** - *merge related columns into one feature*')
-if st.button('Submit'):
-    pass
