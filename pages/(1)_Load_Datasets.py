554f0a24c1e227374688e020ab6e2e910e289c85
unknown <seahzs@gmail.com>
2023-10-05 01:27:13 +0800

    5Oct

--- pages/(1)_Load_Datasets.py
+++ pages/(1)_Load_Datasets.py
@@ -14,17 +14,23 @@ with st.sidebar:
 
 
 # Load datasets
-st.write("### Load Datasets")
-files = st.file_uploader("Upload dataset CSV file(s):", type="csv", accept_multiple_files=True)
-headers = st.checkbox("CSV files has headers", value=True)
-skip_rows = st.text_input("Number of rows to Skip:", placeholder="Enter an integer")
+"### Load Datasets"
+"**Step 1:** Upload CSV file(s)"
+files = st.file_uploader("Upload dataset CSV file(s):", type="csv", accept_multiple_files=True, label_visibility="collapsed")
 if files:
-    for csv_file in files:
-        dataset=csv_file.name.split('.')[0]
-        datasets[dataset] = pd.read_csv(csv_file, skiprows=int(skip_rows) if skip_rows else 0, header='infer' if headers else None)
-        st.session_state["datasets"]=datasets
-        with st.expander(f"**{dataset}** - {datasets[dataset].shape}"):
-            st.write(datasets[dataset].head(3))
-        with st.sidebar:
-            st.info(f"- {dataset}")
-    st.success("To continue, go to **'(2) Data 'Preparation'** on the sidebar.")
+    "---"
+    "**Step 2:** Load dataset(s) from CSV files"
+    headers = st.checkbox("CSV files has headers", value=True)
+    skip_rows = st.text_input("Number of rows to Skip:", placeholder="Enter an integer")
+    if st.button("Load dataset(s)"):
+        "---"
+        "**Step 3:** Verify headers and records"
+        for csv_file in files:
+            dataset=csv_file.name.split('.')[0]
+            datasets[dataset] = pd.read_csv(csv_file, skiprows=int(skip_rows) if skip_rows else 0, header='infer' if headers else None)
+            st.session_state["datasets"]=datasets
+            with st.expander(f"**{dataset}** - {datasets[dataset].shape}"):
+                st.write(datasets[dataset].head(3))
+            with st.sidebar:
+                st.info(f"- {dataset}")
+        st.success("If headers are incorrect, repeat step 2, otherwise proceed to **'(2) Data 'Preparation'**.")
